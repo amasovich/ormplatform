94b59b72800317f2d8e10a24a9009f5d8bf7a79b
Amasovich <amasovich@yandex.ru>
2025-12-09 22:30:14 +0300

    chore: initial Spring Boot project

---
+++ src/main/java/ru/mifi/ormplatform/OrmplatformApplication.java
@@ -0,0 +1,13 @@
+package ru.mifi.ormplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mifi/ormplatform/OrmplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mifi.ormplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
